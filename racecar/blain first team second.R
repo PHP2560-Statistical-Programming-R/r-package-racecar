1a84fece7885d499f3dddb75215b1617bbd62846
blainmorin <blainmorin@brown.edu>
2017-12-06 14:00:26 -0500

    added speed graph

--- racecar/blain first team second.R
+++ racecar/blain first team second.R
@@ -1,9 +1,15 @@
 
 
-mapspeed <- function(data) {
+mapspeed <- function(data, laps, distance) {
+  
+  data <- data %>% filter(lap == laps)
+  
+  
   
   ggplot(data, aes( x = GPS_Latitude, y = GPS_Longitude)) + 
-    geom_point(aes(color = GPS_Speed))
+    geom_point(aes(color = GPS_Speed)) + facet_wrap(aes(~lap))
   
 }
 
+
+airfuel <- function (data, laps)
