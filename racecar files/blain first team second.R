cf45e1c470108bf05f1396cfe06968ac6d61155d
blainmorin <blainmorin@brown.edu>
2017-12-06 15:01:43 -0500

    working function

--- racecar files/blain first team second.R
+++ racecar files/blain first team second.R
@@ -2,9 +2,9 @@
 
 mapspeed <- function(data, laps = 1, startdist = min(data$Distance) , enddist = max(data$Distance)) {
   data %>%
-    filter(data$Lap == laps) %>%
-    filter(startdist <= data$Distance) %>%
-    filter(enddist >= data$Distance) %>%
+    filter(Lap == laps) %>%
+    filter(Distance >= startdist) %>%
+    filter(Distance <= enddist) %>%
     ggplot(aes( x = GPS_Latitude, y = GPS_Longitude)) + 
       geom_point(aes(color = GPS_Speed)) 
   
