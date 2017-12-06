8c6e6acb464f2f2f530eea278a7271774f9a2ce2
jesskaminsky <jessica_kaminsky@brown.edu>
2017-12-06 15:13:05 -0500

    10/06

--- racecar files/jess_graphs.R
+++ racecar files/jess_graphs.R
@@ -1,10 +1,14 @@
-braking_pattern <- function(data, laps, distance) {
-  ggplot(data, aes( x = GPS_Latitude, y = GPS_Longitude)) + 
+braking_pattern <- function(data, laps = 1, startdist = min(data$Distance), enddist = max(data$Distance)) {
+  data %>%
+    filter(Lap == laps) %>%
+    filter(Distance >= startdist) %>%
+    filter(Distance <= enddist) %>%
+  ggplot(aes( x = GPS_Latitude, y = GPS_Longitude)) +
     geom_point(aes(color = BPS_Front))
 }
 
-throttle_position <- function(data, laps, distance) {
-  ggplot(data, aes( x = GPS_Latitude, y = GPS_Longitude)) + 
+throttle_position <- function(data, laps = 1, startdist = min(data$Distance), enddist = max(data$Distance)) {
+  ggplot(data, aes( x = GPS_Latitude, y = GPS_Longitude)) +
     geom_point(aes(color = PE3_TPS))
 }
 
