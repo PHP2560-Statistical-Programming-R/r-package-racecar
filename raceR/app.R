97fefe5b0219e3a830627f910e8433a305341d37
jesskaminsky <jessica_kaminsky@brown.edu>
2017-12-08 14:19:37 -0500

    friday

--- raceR/app.R
+++ raceR/app.R
@@ -1,50 +1,46 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+
+  fluidRow(column(width = 6,
+                  fileInput("upload1", label = h4("Upload .csv here"))),
+           column(width = 6,
+                  fileInput("upload2", label = h4("Upload .csv here")))),
+
+
+  fluidRow(column(width = 4,
+                  selectInput("graphtype2", label = h5("Choose Graph"),
+                              choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3),
+                              selected = 1)),
+
+           column(width = 4,
+                  sliderInput("distrange", label = h5("Select Distance"), min = 0,
+                              max = 100, value = c(40, 60))),
+
+
+           column(width = 4,
+                  selectInput("graphtype2", label = h5("Choose Graph"),
+                              choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3),
+                              selected = 1))),
+
+  fluidRow(column(width = 6,
+                  plotOutput("graph1")),
+           column(width = 6,
+                  plotOutput("graph2")))
+
+
 )
 
-# Define server logic required to draw a histogram
+
 server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+  options(shiny.maxRequestSize = 50*1024^2)
+  output$graph1 <- renderPlot( {
+    input_data <- cleanSingleLap(input$upload1)
+
+    lapspeed(input_data)
+  })
 }
 
-# Run the application 
+
 shinyApp(ui = ui, server = server)
 
--- racecar/R/all_functions.R
+++ racecar/R/all_functions.R
@@ -1,5 +1,5 @@
 #######################cleanSingleLap Function#####################################
-cleanSingleLap <- function(file, lapNum) {
+cleanSingleLap <- function(file, lapNum = 1) {
   lapData <- read_csv(file, skip = 15)
 
   ### This command gets rid of the extra column
@@ -62,7 +62,7 @@ RPM_speed <- function(data, laps = 1){
     filter(Lap == laps) %>%
     mutate(gear_floor = floor(Calculated_Gea)) %>%
     group_by(gear_floor) %>%
-    ggplot(aes(colour = PE3_TPS_)) + 
+    ggplot(aes(colour = PE3_TPS_)) +
     scale_colour_gradientn(colours=rainbow(4))+
     geom_point(aes(x = GPS_Speed, y = PE3_RPM), size = 0.1)+
     facet_wrap(~gear_floor)
@@ -76,8 +76,8 @@ RPM_speed1 <- function(data, laps = 1, startdist = min(Data$Distance), enddist =
     filter(Distance >= startdist & Distance <= enddist) %>%
     ggplot(aes(x = Distance)) +
     geom_line(aes(y = GPS_Speed), color = "#CC79A7", size = 0.8) +
-    theme(panel.grid.minor = element_blank(), 
-          panel.grid.major = element_line(color = "gray50", size = 0.5), 
+    theme(panel.grid.minor = element_blank(),
+          panel.grid.major = element_line(color = "gray50", size = 0.5),
           panel.grid.major.x =element_blank())+
     theme(panel.background = element_blank())+
     scale_y_continuous(expand = c(0,0), limits = c(0, 100)) +
@@ -91,20 +91,20 @@ RPM_speed1 <- function(data, laps = 1, startdist = min(Data$Distance), enddist =
     ggtitle("GPS_Speed\n") +
     labs(x = NULL, y = NULL) +
     theme(plot.title = element_text(hjust = -0.16, vjust = 2.12, colour = "#CC79A7", size = 14))
-  
+
   p2 <- data %>%
     filter(Lap == laps) %>%
     filter(Distance >= startdist & Distance <= enddist) %>%
     ggplot(aes(x = Distance)) +
     geom_line(aes(y = PE3_RPM), color = "#00A4E6", size = 0.8) +
-    theme(panel.grid.minor = element_blank(), 
-          panel.grid.major = element_line(color = "gray50", size = 0.5), 
+    theme(panel.grid.minor = element_blank(),
+          panel.grid.major = element_line(color = "gray50", size = 0.5),
           panel.grid.major.x =element_blank()) +
     theme(panel.background = element_blank()) +
     scale_y_continuous(expand = c(0,0), limits = c(2000, 17000)) +
     scale_x_continuous(breaks = c(0, seq(100,10000,100))) +
     theme(axis.text.y = element_text(size = 10),
-          axis.text.x = element_text(size = 10), 
+          axis.text.x = element_text(size = 10),
           axis.ticks = element_line(colour = 'gray50', size = 0.5),
           axis.ticks.length = unit(.25, "npc"),
           axis.ticks.x = element_line(colour = "black"),
@@ -112,31 +112,31 @@ RPM_speed1 <- function(data, laps = 1, startdist = min(Data$Distance), enddist =
     ggtitle("RPM\n") +
     labs(x = NULL, y = NULL) +
     theme(plot.title = element_text(hjust = -0.16, vjust = 2.12, colour = "#00A4E6", size = 14))
-  
+
   ## make gtable objects from ggplot objects
   ## gtable object shows how grobs are put together to form a ggplot
   g1 <- ggplot_gtable(ggplot_build(p1))
   g2 <- ggplot_gtable(ggplot_build(p2))
-  
+
   ## get the location of the panel of p1
   ## so that the panel os p2 is positioned correctly on top of it
   pp <- c(subset(g1$layout, name == "panel", se = t:r))
   print(pp)
   ## superimpose p2 (the panel) on p1
   grob_p2 <- as.list(g2$grobs[[which(g2$layout$name == "panel")]])
-  
+
   g <- gtable_add_grob(g1, grob_p2, pp$t, pp$l, pp$b, pp$l)
   print(g)
   ## extract the y-axis of p2
   ia <- which(g2$layout$name == "axis-l")
-  
+
   ga <- g2$grobs[[ia]]
   ax <- as.list(ga$children)[[2]]
-  
+
   ## flip it horizontally
   ax$widths <- rev(ax$widths)
   ax$grobs <- rev(ax$grobs)
-  
+
   ## add the flipped y-axis to the right
   g <- gtable_add_cols(g, g2$widths[g2$layout[ia, ]$l], length(g$widths) - 1)
   g <- gtable_add_grob(g, ax, pp$t, length(g$widths) - 1, pp$b)
@@ -164,9 +164,9 @@ mapspeed <- function(data, laps = 1, startdist = min(data$Distance) , enddist = 
     filter(Lap == laps) %>%
     filter(Distance >= startdist) %>%
     filter(Distance <= enddist) %>%
-    ggplot(aes( x = GPS_Latitude, y = GPS_Longitude)) + 
-    geom_point(aes(color = GPS_Speed)) 
-  
+    ggplot(aes( x = GPS_Latitude, y = GPS_Longitude)) +
+    geom_point(aes(color = GPS_Speed))
+
 }
 
 ############### Graph that plots air fuel ratio and rpm ###############################
