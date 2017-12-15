47854508eaf1e36497abe0cbc2c21db61194c887
jesskaminsky <jessica_kaminsky@brown.edu>
2017-12-15 14:33:36 -0500

    clean repo

--- racecar files/fuyu_graghs.R
+++
@@ -1,7 +0,0 @@
-#Graph of RPM vs Speed
-
-#Graph that compares lap speed
-
-#Graph that compares speed among driver
-
-
--- racecar files/plotly test.R
+++
@@ -1,3 +0,0 @@
-devtools::install_github("ropensci/plotly")
-library(plotly)
-plot_ly(z = ~volcano)
